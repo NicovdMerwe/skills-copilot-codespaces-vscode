65db2698c3ea5460be06cced93649edf674aaf69
NicovdMerwe <127262620+NicovdMerwe@users.noreply.github.com>
2024-05-03 02:01:28 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,44 @@
+function skillsMember() {
+  return {
+    id: 'skills',
+    title: 'Skills',
+    icon: 'fa fa-wrench',
+    sections: [
+      {
+        title: 'Technical Skills',
+        items: [
+          'JavaScript',
+          'React',
+          'Redux',
+          'Node.js',
+          'Express.js',
+          'MongoDB',
+          'MySQL',
+          'HTML',
+          'CSS',
+          'SASS/SCSS',
+          'Webpack',
+          'Babel',
+          'Git',
+          'Jest',
+          'Enzyme',
+          'Mocha',
+          'Chai',
+          'Travis CI',
+          'Heroku',
+        ],
+      },
+      {
+        title: 'Soft Skills',
+        items: [
+          'Teamwork',
+          'Problem Solving',
+          'Time Management',
+          'Leadership',
+          'Communication',
+          'Adaptability',
+        ],
+      },
+    ],
+  };
+}
