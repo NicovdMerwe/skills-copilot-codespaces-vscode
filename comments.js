3a6738d38d95f3146b8ba7969b3393308e2d4227
NicovdMerwe <127262620+NicovdMerwe@users.noreply.github.com>
2024-05-03 02:22:01 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,7 +1,21 @@
-// Creates web server that listens for HTTP requests and responds with a JSON object
-// containing a list of comments.
-//
-// Usage:
-// 1. Run the server
-// 2. Open a web browser and go to http://localhost:3000/comments
-//
+// create web server that listens to port 3000
+// when a user sends a request to the server, the server should respond with the content of the comments.json file
+
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  fs.readFile('comments.json', 'utf8', (err, data) => {
+    if(err) {
+      res.writeHead(500);
+      res.end('An error occurred on the server.');
+      return;
+    }
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(data);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
