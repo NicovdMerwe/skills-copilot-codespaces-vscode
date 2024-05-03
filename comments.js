6f232ab85482fb388d687a58cfb1db89cf7b92bc
NicovdMerwe <127262620+NicovdMerwe@users.noreply.github.com>
2024-05-03 02:57:07 +0000

    Copilot third commit2

--- comments.js
+++
@@ -1,9 +0,0 @@
-// create web server with express
-// create a route /comments
-// create a route /comments/new
-// create a route /comments/:id
-// create a route /comments/:id/edit
-// create a route /comments/:id/delete
-
-const express = require('express');
-const app = express
